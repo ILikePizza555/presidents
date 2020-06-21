86d1cd2a73fd2f99721dd95db9e0e7ba008aacce
Izzy <avrisaac555@gmail.com>
2020-06-21 14:35:35 -0700

    Fixed sendfile

--- srv/app.js
+++ srv/app.js
@@ -4,7 +4,7 @@ const http = require("http")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.sendFile("html/index.html")
+    res.sendFile("./html/index.html", {root: process.cwd()})
 })
 
 const server = http.createServer(app).listen(8000)
