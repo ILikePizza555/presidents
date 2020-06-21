d26f2187154eae72bd35337714e196eabf99b0bb
Izzy <avrisaac555@gmail.com>
2020-06-21 14:32:03 -0700

    Added html to express app

--- srv/app.js
+++ srv/app.js
@@ -1,3 +1,10 @@
 const express = require("express")
+const http = require("http")
 
 const app = express()
+
+app.get("/", (req, res) => {
+    res.sendFile("html/index.html")
+})
+
+const server = http.createServer(app).listen(8000)
