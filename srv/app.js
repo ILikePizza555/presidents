6a67c99ddc24b0c0d718fbd256253010c5b5c315
Izzy <avrisaac555@gmail.com>
2020-06-21 14:52:00 -0700

    Added static files to server

--- srv/app.js
+++ srv/app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const http = require("http")
 
-const app = express()
+const app = express();
+app.use("/static", express.static("static_assets"));
 
 app.get("/", (req, res) => {
-    res.sendFile("./html/index.html", {root: process.cwd()})
-})
+    res.sendFile("./html/index.html", {root: process.cwd()});
+});
 
-const server = http.createServer(app).listen(8000)
+const server = http.createServer(app).listen(8000);
